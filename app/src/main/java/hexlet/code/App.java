5f7d159578dd98ea70fea329a72181803c50795f
Alexander Kamenetsky <svdegron@gmail.com>
2023-05-24 16:21:09 +0500

    Reading from console
    Add Scanner and class Cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
 		System.out.println("Welcome to the Brain Games!");
+
+		Cli.greeting();
+		String userName = scanner.next();
+
+		Cli user = new Cli(userName);
+
+		System.out.println(String.format("Hello, %s!", user.getName()));
+
+		scanner.close();
 	}
 }
