55fded4ea4891d4882bdea7d4a13ddc1af7d4364
Alexander Kamenetsky <svdegron@gmail.com>
2023-05-28 16:21:34 +0500

    5 step
    first game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,20 +1,55 @@
 package hexlet.code;
 
+//import java.util.Random;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Cli user;
 
-        System.out.println("Welcome to the Brain Games!");
+        printMenu();
 
+        int chooseItem = scanner.nextInt();
+
+        switch (chooseItem) {
+            case 1:
+                createUser(scanner);
+                break;
+            case 2:
+                user = createUser(scanner);
+                Eval newGame = new Eval(user);
+                newGame.startGame(scanner);
+                break;
+            default:
+                System.out.println(String.format("Menu item with number \"%s\" is not in the list", chooseItem));
+                break;
+        }
+
+        scanner.close();
+    }
+
+    public static void printMenu() {
+        StringBuilder menu = new StringBuilder();
+
+        menu.append("Please enter the game number and press Enter.\n");
+        menu.append("1 - Greet\n");
+        menu.append("2 - Even\n");
+        menu.append("0 - Exit\n");
+        menu.append("Your choice: ");
+
+        System.out.print(menu.toString());
+    }
+
+    public static Cli createUser(Scanner scanner) {
         Cli.greeting();
 
         String userName = scanner.next();
+
         Cli user = new Cli(userName);
 
         System.out.println(String.format("Hello, %s!", user.getName()));
 
-        scanner.close();
+        return user;
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,6 +3,9 @@ package hexlet.code;
 public class Cli {
     private String name;
 
+    Cli() {
+    }
+
     Cli(String name) {
         this.name = name;
     }
@@ -11,7 +14,12 @@ public class Cli {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public static void greeting() {
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
     }
 }
